aef8fe34d2c48e755f2e8e2a081b52a3b46855ed
Bairon Chirinos <bylexprogrammer@gmail.com>
2023-05-27 22:17:28 -0400

    Add - Button Scroll Up

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,4 +20,16 @@ navLink.forEach((link) =>
     })
 );
 
+/*=============== SCROLL UP =============== */
+
+const scrollUp = document.querySelector('#scroll-up');
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
 
