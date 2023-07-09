5d369668c32fd41f7640ba489d38fb854fad8705
Bairon Chirinos <bylexprogrammer@gmail.com>
2023-07-08 23:12:35 -0400

    🔧Fix - Delete Project and Script Arrow Up

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,16 +20,3 @@ navLink.forEach((link) =>
     })
 );
 
-/*=============== SCROLL UP =============== */
-
-const scrollUp = document.querySelector('#scroll-up');
-
-scrollUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-});
-
-
